0f980f01633bceec3a7ec4855fcea21689aa7f02
DASARI SIVA SAI SURYA TEJA <110438431+suryatej007@users.noreply.github.com>
2025-04-11 23:50:39 +0530

    Update lstm_train.py

--- lstm_train.py
+++ lstm_train.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler  
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.models import Sequential
-model = Sequential()
 from tensorflow.keras.layers import LSTM, Dense, Dropout  
 import joblib  
 
